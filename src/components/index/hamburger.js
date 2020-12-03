96239c62efd6dca27ef3cc1f455a4afeaf768fa8
Vlad_Nozhkin <mega.nozhkin@mail.ru>
2020-12-03 22:39:57 +0300

    added hamburger

---
+++ src/components/index/hamburger.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const hamburger = document.querySelector('#hamburger');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', () => menu.classList.toggle('open_menu'));
+
