dcfaad5182298256c10b5cbd12d6be628c3d9a9f
Vlad Nozhkin <mega.nozhkin@mail.ru>
2020-12-08 18:08:33 +0300

    stylized range

---
+++ src/components/product/range.js
@@ -0,0 +1,13 @@
+'use strict';
+
+$("#formRange").ionRangeSlider({
+    type: 'double',
+    min: 0,
+    max: 800,
+    from: 52,
+    to: 400,
+    grid: false,
+    grid_margin: false,
+    prefix: '$',
+    skin: "flat",
+});
