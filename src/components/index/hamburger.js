4102d72c288516b5972eb19defad27c7d46a6921
Vladislav Nozhkin <mega.nozhkin@mail.ru>
2022-03-01 15:17:51 +0300

    init vue app

--- src/components/index/hamburger.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const hamburger = document.querySelector('#hamburger');
-const menu = document.querySelector('.menu');
-
-hamburger.addEventListener('click', () => menu.classList.toggle('open_menu'));
-
